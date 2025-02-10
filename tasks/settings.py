2cabd08da77608d2235ef551103b120127346bcb
Enoch Jason J <enochjasonj@Enochs-MacBook-Air.local>
2025-02-10 21:31:54 +0530

    Update ALLOWED_HOSTS for Render

--- tasks/settings.py
+++ tasks/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.255','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['https://backend-taskmanager-5aqf.onrender.com/','192.168.0.255','localhost','127.0.0.1']
 
 
 # Application definition
